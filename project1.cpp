59d09b1b1d4fc2c4c335ab8bc220e2742ac777fc
Mason McDaniel <mcdaniel.121@wright.edu>
2024-01-18 00:15:57 -0500

    For some reason if the number of dice roles is > 10 it breaks

--- project1.cpp
+++ project1.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main() {
     int rolls = 0;
+    
     cout << "How many rolls?" << endl;
     cin >> rolls;
     cout << "Simulating " << rolls << " rolls..." << endl;
@@ -17,10 +18,16 @@ int main() {
     srand(time(NULL));
 
     for (int i = 0; i < rolls; i++){
-        array[i] = (rand() % 11) + 2;
+        array[i] = (rand() % 12) + 2;
     }
-    for (int i = 0; i < rolls; i++){
-        cout << array[i] << endl;
+    for (int j = 0; j < 11; j++){
+        int count = 0;
+        for(int k = 0; k < rolls; k++){
+            if((j+2) == array[k]){
+            count++;
+            }
+        }
+        cout << (j+2) << " was rolled " << count << " times" << endl;
     }
-
+    
 }
